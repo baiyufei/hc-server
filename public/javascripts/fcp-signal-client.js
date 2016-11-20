8e043e17873f033aa145620459338be823465d03
baiyufei <baiyufei@outlook.com>
2016-11-20 11:39:34 +0800

    signal client

---
+++ public/javascripts/fcp-signal-client.js
@@ -0,0 +1,46 @@
+function fcpSignalClient(address) {
+  var ioClient = io.connect(address + '/fcpSignal');
+  var sound = fcpSound();
+  var rtc;
+
+  var State = {
+    OFF_LINE: 0,
+    NOT_JOINED: 1,
+    USUAL:    2,
+    CALL_WAIT: 3,
+    CALL_RING: 4,
+    CALL_CONNECTED: 5,
+    CALL_GROUP: 6
+  };
+
+  var uid;        // local client uid
+  var state = State.OFF_LINE;
+
+  ioClient.on('login-answer', function(msg) {
+    if (msg.success) {
+      uid = msg.uid;
+      state = State.USUAL;
+      rtc = webRTCClient(ioClient);
+      sound.play('login-success');
+    } else {
+      state = State.NOT_JOINED;
+      if (msg.reason === "no name") {
+        sound.play('no-user');
+      }
+      else if (msg.reason === "duplicate") {
+        sound.play('duplication');
+      }
+      else if (msg.reason === "'no verification") {
+        sound.play('no-card');
+      }
+    }
+  });
+
+  var that = {};
+
+  that.nameLogin = function(name) {
+    ioClient.emit('name-login', {'name': name});
+  };
+
+  return that;
+}
