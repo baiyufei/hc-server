9cf0435b888d18b4d47055167e4e4201b2c2fd3d
baiyufei <baiyufei@outlook.com>
2016-11-20 11:32:21 +0800

    sound

---
+++ public/javascripts/fcp-sound.js
@@ -0,0 +1,39 @@
+function fcpSound() {
+  var names = ['busy','duplication','hangup','login-success','no-card','offline','refuse','no-user'];
+  var audios = {};
+  var audioNow = null;
+
+  function addAudio(name, loop) {
+    var audio = new Audio('sound/' + name +'.mp3');
+    if (loop) {
+      audio.loop = true;
+    }
+    audios[name] = audio;
+  }
+
+  for (var i = 0; i < names.length; i++) {
+    addAudio(names[i], false);
+  }
+  addAudio('ring', true);
+  addAudio('wait', true);
+
+  return {
+    'play': function(name) {
+      if (audioNow !== null) {
+        audioNow.pause();
+        audioNow.currentTime = 0;
+      }
+      audioNow = audios[name];
+      if (audioNow !== undefined) {
+        audioNow.play();
+      }
+    },
+    'stop': function() {
+      if (audioNow !== null) {
+        audioNow.pause();
+        audioNow.currentTime = 0;
+        audioNow = null;
+      }
+    }
+  };
+}
